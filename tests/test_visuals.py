1c40263d5ebc13da34255904a1f89b318935a1a4
Jacan Chaplais <jacan.chaplais@gmail.com>
2025-07-09 14:31:13 +0100

    test: increase timeout for visual test

--- tests/test_visuals.py
+++ tests/test_visuals.py
@@ -7,6 +7,6 @@ PARENT_DIR = Path(__file__).parent
 
 def test_pyvista_vis() -> None:
     """Tests if PyVista visuals are running without throwing exceptions."""
-    app = AppTest.from_file(PARENT_DIR / "pyvista_vis.py", default_timeout=10)
+    app = AppTest.from_file(PARENT_DIR / "pyvista_vis.py", default_timeout=60)
     app.run()
     assert not app.exception
