3ff34629c6b1e48d5790123b7ae8261d9424b5ab
Jacan Chaplais <jacan.chaplais@gmail.com>
2025-07-09 10:43:57 +0100

    test: removed ifmain guard from test

--- tests/pyvista_vis.py
+++ tests/pyvista_vis.py
@@ -2,28 +2,22 @@
 import pyvista as pv
 from stpyvista.trame_backend import stpyvista
 
+## Initialize a plotter object
+plotter = pv.Plotter(window_size=[400, 400])
 
-def main() -> None:
-    ## Initialize a plotter object
-    plotter = pv.Plotter(window_size=[400, 400])
+## Create a mesh with a cube
+mesh = pv.Cube()
 
-    ## Create a mesh with a cube
-    mesh = pv.Cube()
+## Add some scalar field associated to the mesh
+mesh["my_scalar"] = mesh.points[:, 2] * mesh.points[:, 0]
 
-    ## Add some scalar field associated to the mesh
-    mesh["my_scalar"] = mesh.points[:, 2] * mesh.points[:, 0]
+## Add mesh to the plotter
+plotter.add_mesh(mesh, scalars="my_scalar", cmap="bwr")
 
-    ## Add mesh to the plotter
-    plotter.add_mesh(mesh, scalars="my_scalar", cmap="bwr")
+## Final touches
+plotter.view_isometric()
+plotter.add_scalar_bar()
+plotter.background_color = "white"
 
-    ## Final touches
-    plotter.view_isometric()
-    plotter.add_scalar_bar()
-    plotter.background_color = "white"
-
-    ## Pass a key to avoid re-rendering at each page change
-    stpyvista(plotter, key="pv_cube")
-
-
-if __name__ == "__main__":
-    main()
+## Pass a key to avoid re-rendering at each page change
+stpyvista(plotter, key="pv_cube")
