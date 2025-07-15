pasteurlabs/tesseract-streamlit

commit b92ebbce93ae4018a661535a4f602d91e626c73d

feat(cli): enforce use of .py ext, or stdout (#25)
#### Description of changes
CLI now throws a `click.UsageError` if `OUTPUT` is either not passed
with a `.py` extension, or is not `stdout` via passing a hyphen `-`.
This has been enforced because Streamlit expects a `.py` extension to
run with the scripts it serves.

#### Testing done
Added an automated test to check that the error is thrown when the
extension is missing, and matches against the specific error message.

tesseract_streamlit/cli.py
@@ -43,8 +43,17 @@ def main(
     URL is the address to the Tesseract you'd like to generate your
     interface for.
 
-    OUTPUT is the file location to write the Streamlit app script.
+    OUTPUT is the file location to write the Streamlit app script. Must
+    have a '.py' file extension, or be a dash '-' to pipe to stdout.
+
+    The generated script can then be passed to the 'streamlit run'
+    command to serve the app.
     """
+    if not (output.name.endswith(".py") or (output.name == "<stdout>")):
+        raise click.UsageError(
+            "OUTPUT must either be '-' (stdout), or a script name ending with "
+            "a '.py' extension."
+        )
     test_var = os.getenv("TESSERACT_STREAMLIT_TESTING", default="0")
     test = test_var.lower() in {"1", "yes", "true", "on", "enabled"}
     env = Environment(

tests/test_cli.py
@@ -22,6 +22,20 @@ def test_cli(goodbyeworld_url: str) -> None:
         assert Path(app_path).exists()
 
 
+def test_py_extension(goodbyeworld_url: str) -> None:
+    """Checks that exception raised if not using '.py' extension."""
+    err_msg = (
+        "Error: OUTPUT must either be '-' (stdout), or a script name ending "
+        "with a '.py' extension."
+    )
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as temp_dir:
+        app_path = f"{temp_dir}/app"
+        result = runner.invoke(main, [goodbyeworld_url, app_path])
+        assert result.exit_code != 0
+        assert err_msg in result.stderr
+
+
 def test_app(goodbyeworld_url: str) -> None:
     runner = CliRunner()
     result = runner.invoke(main, [goodbyeworld_url, "-"])

commit 34e07df8db12274bc4e43d373a2fcd1f4384cce9

refactor(cli): migrate from click to typer (#30)
#### Description of changes

Refactor the CLI from a `click` app to a `typer` app. Migration is just
to make the aesthetic experience of Tesseract Streamlit similar to
Tesseract Core.

> [!NOTE]
> Since `rich` is now used to print the error message introduced in #25,
the control codes make it harder to perform substring matches against
the error message. So, this check has been removed from the automated
test (but the test is still in place to check that an error is thrown).

#### Testing done

Automated tests pass, and manual tests show more consistent visual style
with Tesseract Core.

pyproject.toml
@@ -26,7 +26,7 @@ dynamic = ["version"]
 Homepage = "https://github.com/pasteurlabs/tesseract-streamlit"
 
 [project.scripts]
-tesseract-streamlit = "tesseract_streamlit.cli:main"
+tesseract-streamlit = "tesseract_streamlit.cli:cli"
 
 [project.optional-dependencies]
 docs = [

tesseract_streamlit/cli.py
@@ -4,48 +4,59 @@ Defines the command-line interface for ``tesseract-streamlit``.
 The main entrypoint of this module is the ``main()`` function.
 """
 
-import io
 import os
+import typing
 from pathlib import Path
 
-import click
+import typer
 from jinja2 import Environment, FileSystemLoader
+from rich.console import Console
 
 from tesseract_streamlit.parse import extract_template_data
 
 PACKAGE_DIR = Path(__file__).parent
 
+err_console = Console(stderr=True)
+cli = typer.Typer()
 
-@click.command()
-@click.argument("url", type=click.STRING)
-@click.argument("output", type=click.File("wt"))
-@click.option(
-    "-u",
-    "--user-code",
-    type=click.Path(exists=True, path_type=Path),
-    help="User defined functions for plotting inputs / outputs of the Tesseract.",
-)
+
+@cli.command()
 def main(
-    url: str,
-    output: io.TextIOWrapper,
-    user_code: Path | None,
+    url: typing.Annotated[
+        str,
+        typer.Argument(help="Address to the Tesseract to use in the app."),
+    ],
+    output: typing.Annotated[
+        typer.FileTextWrite,
+        typer.Argument(
+            help=(
+                "File location to write the Streamlit app script. Must have a "
+                "'.py' file extension, or be a dash '-' to pipe to stdout."
+            )
+        ),
+    ],
+    user_code: typing.Annotated[
+        Path | None,
+        typer.Option(
+            help=(
+                "User defined functions for plotting inputs / outputs of the Tesseract."
+            ),
+            exists=True,
+        ),
+    ] = None,
 ) -> None:
     """Generates a Streamlit app from Tesseract OpenAPI schemas.
-
-    URL is the address to the Tesseract you'd like to generate your
-    interface for.
-
-    OUTPUT is the file location to write the Streamlit app script. Must
-    have a '.py' file extension, or be a dash '-' to pipe to stdout.
 
     The generated script can then be passed to the 'streamlit run'
     command to serve the app.
     """
     if not (output.name.endswith(".py") or (output.name == "<stdout>")):
-        raise click.UsageError(
+        err_console.print(
+            "[bold red]Error: [/bold red]"
             "OUTPUT must either be '-' (stdout), or a script name ending with "
-            "a '.py' extension."
+            "a '.py' extension. Aborting."
         )
+        raise typer.Exit(code=2)
     test_var = os.getenv("TESSERACT_STREAMLIT_TESTING", default="0")
     test = test_var.lower() in {"1", "yes", "true", "on", "enabled"}
     env = Environment(

tests/test_cli.py
@@ -3,10 +3,10 @@ import tempfile
 from pathlib import Path
 
 import orjson
-from click.testing import CliRunner
 from streamlit.testing.v1 import AppTest
+from typer.testing import CliRunner
 
-from tesseract_streamlit.cli import main
+from tesseract_streamlit.cli import cli
 
 PARENT_DIR = Path(__file__).parent
 os.environ["TESSERACT_STREAMLIT_TESTING"] = "1"
@@ -16,7 +16,7 @@ def test_cli(goodbyeworld_url: str) -> None:
     runner = CliRunner()
     with tempfile.TemporaryDirectory() as temp_dir:
         app_path = f"{temp_dir}/app.py"
-        result = runner.invoke(main, [goodbyeworld_url, app_path])
+        result = runner.invoke(cli, [goodbyeworld_url, app_path])
         assert result.exit_code == 0
         assert result.output == ""
         assert Path(app_path).exists()
@@ -24,21 +24,16 @@ def test_cli(goodbyeworld_url: str) -> None:
 
 def test_py_extension(goodbyeworld_url: str) -> None:
     """Checks that exception raised if not using '.py' extension."""
-    err_msg = (
-        "Error: OUTPUT must either be '-' (stdout), or a script name ending "
-        "with a '.py' extension."
-    )
     runner = CliRunner()
     with tempfile.TemporaryDirectory() as temp_dir:
         app_path = f"{temp_dir}/app"
-        result = runner.invoke(main, [goodbyeworld_url, app_path])
+        result = runner.invoke(cli, [goodbyeworld_url, app_path])
         assert result.exit_code != 0
-        assert err_msg in result.stderr
 
 
 def test_app(goodbyeworld_url: str) -> None:
     runner = CliRunner()
-    result = runner.invoke(main, [goodbyeworld_url, "-"])
+    result = runner.invoke(cli, [goodbyeworld_url, "-"])
     assert result.exit_code == 0
     assert result.output != ""
     app = AppTest.from_string(result.output, default_timeout=3)
@@ -58,7 +53,7 @@ def test_app(goodbyeworld_url: str) -> None:
 
 def test_zerodim_pprint(zerodim_url: str) -> None:
     runner = CliRunner()
-    result = runner.invoke(main, [zerodim_url, "-"])
+    result = runner.invoke(cli, [zerodim_url, "-"])
     assert result.exit_code == 0
     assert result.output != ""
     app = AppTest.from_string(result.output, default_timeout=3)
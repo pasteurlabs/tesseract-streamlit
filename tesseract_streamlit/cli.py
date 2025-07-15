34e07df8db12274bc4e43d373a2fcd1f4384cce9
Jacan Chaplais <31956735+jacanchaplais@users.noreply.github.com>
2025-07-15 16:35:06 +0100

    refactor(cli): migrate from click to typer (#30)
    #### Description of changes
    
    Refactor the CLI from a `click` app to a `typer` app. Migration is just
    to make the aesthetic experience of Tesseract Streamlit similar to
    Tesseract Core.
    
    > [!NOTE]
    > Since `rich` is now used to print the error message introduced in #25,
    the control codes make it harder to perform substring matches against
    the error message. So, this check has been removed from the automated
    test (but the test is still in place to check that an error is thrown).
    
    #### Testing done
    
    Automated tests pass, and manual tests show more consistent visual style
    with Tesseract Core.

--- tesseract_streamlit/cli.py
+++ tesseract_streamlit/cli.py
@@ -4,48 +4,59 @@ Defines the command-line interface for ``tesseract-streamlit``.
 The main entrypoint of this module is the ``main()`` function.
 """
 
-import io
 import os
+import typing
 from pathlib import Path
 
-import click
+import typer
 from jinja2 import Environment, FileSystemLoader
+from rich.console import Console
 
 from tesseract_streamlit.parse import extract_template_data
 
 PACKAGE_DIR = Path(__file__).parent
 
+err_console = Console(stderr=True)
+cli = typer.Typer()
 
-@click.command()
-@click.argument("url", type=click.STRING)
-@click.argument("output", type=click.File("wt"))
-@click.option(
-    "-u",
-    "--user-code",
-    type=click.Path(exists=True, path_type=Path),
-    help="User defined functions for plotting inputs / outputs of the Tesseract.",
-)
+
+@cli.command()
 def main(
-    url: str,
-    output: io.TextIOWrapper,
-    user_code: Path | None,
+    url: typing.Annotated[
+        str,
+        typer.Argument(help="Address to the Tesseract to use in the app."),
+    ],
+    output: typing.Annotated[
+        typer.FileTextWrite,
+        typer.Argument(
+            help=(
+                "File location to write the Streamlit app script. Must have a "
+                "'.py' file extension, or be a dash '-' to pipe to stdout."
+            )
+        ),
+    ],
+    user_code: typing.Annotated[
+        Path | None,
+        typer.Option(
+            help=(
+                "User defined functions for plotting inputs / outputs of the Tesseract."
+            ),
+            exists=True,
+        ),
+    ] = None,
 ) -> None:
     """Generates a Streamlit app from Tesseract OpenAPI schemas.
-
-    URL is the address to the Tesseract you'd like to generate your
-    interface for.
-
-    OUTPUT is the file location to write the Streamlit app script. Must
-    have a '.py' file extension, or be a dash '-' to pipe to stdout.
 
     The generated script can then be passed to the 'streamlit run'
     command to serve the app.
     """
     if not (output.name.endswith(".py") or (output.name == "<stdout>")):
-        raise click.UsageError(
+        err_console.print(
+            "[bold red]Error: [/bold red]"
             "OUTPUT must either be '-' (stdout), or a script name ending with "
-            "a '.py' extension."
+            "a '.py' extension. Aborting."
         )
+        raise typer.Exit(code=2)
     test_var = os.getenv("TESSERACT_STREAMLIT_TESTING", default="0")
     test = test_var.lower() in {"1", "yes", "true", "on", "enabled"}
     env = Environment(
