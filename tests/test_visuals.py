e28b12ff05ad8b354f53d8ae357a32eb955e1f81
Jacan Chaplais <jacan.chaplais@gmail.com>
2026-03-25 10:45:50 +0000

    test: test that the imports and multiprocessing lines work

--- tests/test_visuals.py
+++ tests/test_visuals.py
@@ -13,5 +13,10 @@ PARENT_DIR = Path(__file__).parent
 #     app.run()
 #     assert not app.exception
 
-def test_import_pyvista() -> None:
+def test_visual_imports() -> None:
+    import multiprocessing
+
+    multiprocessing.set_start_method("fork", force=True)
+
     import pyvista as pv
+    from stpyvista import stpyvista
